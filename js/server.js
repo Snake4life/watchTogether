e8993844a71215f1fcd9d5de4ae2c33084c012b2
Rjblaauboer <rj.blaauboer@gmail.com>
2016-03-21 09:55:18 +0000

    Add youtube Iframe player and control of players in other windows

--- js/server.js
+++ js/server.js
@@ -19,6 +19,10 @@ socket.on('chat message', function(msg){
 		socket.emit('chat message', "Notification - Received your ready message");
 		//and send a notification to all other sockets. 
 		socket.broadcast.emit('chat message', "Notification - Another socket ready");
+		socket.broadcast.emit('start video', "Start it");
+	} if(msg === "playVideo" || msg === "stopVideo" || msg === "pauseVideo"){
+		socket.emit('chat message', "Notification - Received your " + msg + "command");
+		socket.broadcast.emit('videoCmd', msg);
 	} else {
 		//else just emit the received message to everyone including the sender
 		io.emit('chat message', msg);
